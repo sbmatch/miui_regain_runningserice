2844638bbfe120ef0b87905b9afbd0340c6721e9
root <maping@mpcloud.top>
2023-11-17 18:06:42 +0800

    无事发生

--- app/src/main/java/com/ma/bitchgiveitback/MainActivity.java
+++ app/src/main/java/com/ma/bitchgiveitback/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 
 }
